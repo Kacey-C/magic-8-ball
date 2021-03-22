7683f45b1e57481c4658be6932637083a8843b5e
kacey_c <kaceycreative@gmail.com>
2021-03-22 15:44:06 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+var magic8Ball = {};
+magic8Ball.listOfAnswers = ["Absolutely", "Without a doubt", "I guess so", "Yes", "Possibly", "I dunno maybe", "Not now please", "why?", "sorry not a chance", "bye bye", "uh what did you say?"];
+
+$("#answer").hide();
+
+magic8Ball.askQuestion = function(question) {
+	$("#answer").fadeIn(4000);
+
+	var random = Math.random();
+	var array = random * this.listOfAnswers.length;
+	var index = Math.floor(array);
+	var answer = this.listOfAnswers[index];
+	$("#answer").text(answer);
+
+	console.log(question);
+	console.log(answer);
+
+	$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+};
+
+$("#answer").hide();
+
+
+var onClick = function(){
+	$("#answer").hide();
+
+	$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+	
+	setTimeout(
+		function(){
+	var question = prompt("What is your question?")
+
+	$("#8ball").effect("shake")
+	magic8Ball.askQuestion(question)}, 500);
+	
+};
+
+$("#questionButton").click(onClick);
+
+
+});
